cd0d938457e9d219fad2524930c192a5ac1e8aed
H3adsho0ot <niklas.roll123@gmail.com>
2014-09-11 10:52:55 +0200

    Changed IP to Niklas PC for testing

--- BomberCowClient/Tester.cs
+++ BomberCowClient/Tester.cs
@@ -22,7 +22,7 @@ namespace BomberCowClient
             Console.Write("Enter your name: ");
             String name = Console.ReadLine();
 
-            if (client.connect("172.25.66.17", 45454))
+            if (client.connect("172.25.66.26", 45454))
             {
                 client.send(name + " joined");
                 Console.WriteLine("You joined the server");
